cd85706768bf7f30587e3bb217133bd786da231b
sjilongarchitect@gmail.com <s7231163>
2013-04-28 23:07:09 -0500

    homework finsihed

--- homework/config/routes.rb
+++ homework/config/routes.rb
@@ -13,4 +13,17 @@ MoviesApp::Application.routes.draw do
 
   delete '/movies/:id', controller: 'movies', action: 'destroy'
 
+  get '/theaters', controller: 'theaters', action: 'index', as: 'theaters'
+
+  get '/theaters/new', controller: 'theaters', action: 'new', as: 'new_theater'
+
+  get '/theaters/:id', controller: 'theaters', action: 'show', as: 'theater'
+
+  get '/theaters/:id/edit', controller: 'theaters', action: 'edit', as: 'edit_theater'
+  put '/theaters/:id', controller: 'theaters', action: 'update'
+
+  post '/theaters', controller: 'theaters', action: 'create'
+
+  delete '/theaters/:id', controller: 'theaters', action: 'destroy'
+
 end
---
+++ homework/db/migrate/20130429035033_theater.rb
@@ -0,0 +1,10 @@
+class Theater < ActiveRecord::Migration
+  def change
+    create_table :theaters do |t|
+
+      t.string :name
+      t.string :address
+      t.string :phone_number
+    end
+  end
+end
